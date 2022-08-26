2fb4297e86eed41e4b3af812317c2b1d5e161f4d
paulmwatson <github.com@paulmwatson.com>
2022-08-26 13:42:29 +0200

    Upgrade parcel and js-worker-search

--- src/js/d3.js
+++ src/js/d3.js
@@ -1,12 +1,8 @@
-import { json } from 'd3-fetch';
-import { hierarchy, treemap } from 'd3-hierarchy';
-import {
-  data, enter, select, selectAll, style,
-} from 'd3-selection';
+import { json } from "d3-fetch";
+import { hierarchy, treemap } from "d3-hierarchy";
+import { select, selectAll, style } from "d3-selection";
 
 export const d3 = {
-  data,
-  enter,
   json,
   hierarchy,
   select,
